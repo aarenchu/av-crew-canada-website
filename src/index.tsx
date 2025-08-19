48760821158c58662e6d99607ab430d3b9d28133
Aaren Chu <aarenchu@gmail.com>
2025-08-19 11:30:07 -0400

    fix: issues with deploymen

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <CssBaseline />
         <App />
       </BrowserRouter>
